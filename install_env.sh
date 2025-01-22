16052ee66220474af7568ce872587a4155c3f078
question406 <sjcs_jijiabao@sjtu.edu.cn>
2025-01-21 22:19:01 -0800

    update requirements

--- install_env.sh
+++ install_env.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+version=12.4
 
 conda create -n rlhf python=3.12
+conda install pytorch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 pytorch-cuda=$version -c pytorch -c nvidia
+conda install -c f"nvidia/label/cuda-{$version}.0" cuda-toolkit -y
 pip install flash-attn --no-build-isolation
-conda install -c "nvidia/label/cuda-12.1.0" cuda-toolkit -y
 pip install vllm
 pip install -e .
+
+pip install sympy regex
